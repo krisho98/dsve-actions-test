7fdd95664c47ec0090dd358c2814ad44f13a123c
Kristoffer Holm <kristoffer.holm@bits.no>
2025-05-12 18:38:16 +0200

    doc: Github Actions test 31

---
+++ .github/workflows/scripts/release-and-publish/createTag.ps1
@@ -0,0 +1,26 @@
+$RepositoryHost = $Env:RepositoryHost.Substring($Env:RepositoryHost.indexOf("/") + 1)
+$RepositoryURI = "$Env:RepositoryHost/$Env:RepositoryURI"
+$CurrentRelease = gh release view --repo $RepositoryURI --json name,tagName,createdAt,body | ConvertFrom-Json
+
+Write-Output "::notice::Latest release in this repository was a release with name: $($CurrentRelease.name), tag version: $($CurrentRelease.tagName)"
+
+$MajorVersionNumber = [int]$CurrentRelease.tagName.split(".")[0]
+$NormalVersionNumber = [int]$CurrentRelease.tagName.split(".")[1]
+$MinorVersionNumber = [int]$CurrentRelease.tagName.split(".")[3]
+
+switch ($Env:ReleaseType) {
+  fix {
+    $MinorVersionNumber = $MinorVersionNumber + 1
+  }
+  feat {
+    $NormalVersionNumber = $NormalVersionNumber + 1
+  }
+  breaking {
+    $MajorVersionNumber = $MajorVersionNumber + 1
+  }
+}
+
+$NewTagVersion = "$MajorVersionNumber.$NormalVersionNumber.$MinorVersionNumber"
+
+Write-Output "::notice::New release will be created with version and tag name: $NewTagVersion"
+
--- .github/workflows/scripts/release-and-publish/determineReleaseType.ps1
+++ .github/workflows/scripts/release-and-publish/determineReleaseType.ps1
@@ -1,19 +1,19 @@
 $CommitMessage = git log --format=%B -n 1 HEAD
-$RepositoryHost = $Env:RepositoryHost.Substring($Env:RepositoryHost.indexOf("/") + 1)
-$RepositoryURI = "$Env:RepositoryHost/$Env:RepositoryURI"
-$CurrentRelease = gh release view --repo $RepositoryURI --json name,tagName,createdAt,body | ConvertFrom-Json
-
-Write-Output "::notice::Latest release in this repository was a release with name: $($CurrentRelease.name)"
+$releaseType = ""
 
 if ( $CommitMessage -match '^doc:.*$') {
-  Add-Content -Value "releaseType=doc" -Path $Env:GITHUB_OUTPUT -Encoding utf8
+  $releaseType = 'doc'
 } elseif ( $CommitMessage -match '^fix:(\s|\S)*$' ) {
-  Add-Content -Value "releaseType=fix" -Path $Env:GITHUB_OUTPUT -Encoding utf8
+  $releaseType = 'fix'
 } elseif ( $CommitMessage -match '^feat:(\s|\S)*$' ) {
-  Add-Content -Value "releaseType=feat" -Path $Env:GITHUB_OUTPUT -Encoding utf8
+  $releaseType = 'feat'
 } elseif ( $CommitMessage -match '^feat!:(\s|\S)*$' ) {
-  Add-Content -Value "releaseType=breaking" -Path $Env:GITHUB_OUTPUT -Encoding utf8
+  $releaseType = 'breaking'
 } else {
   Write-Output "::error title=Naming-Convention-Error::The commit message did not follow the agreed upon naming convention, the name of the lates commit was: $CommitMessage"
   exit 1
 }
+
+Write-Output "::notice::The merge was determined to be a $releaseType change"
+
+Add-Content -Value "releaseType=$releaseType" -Path $Env:GITHUB_OUTPUT -Encoding utf8
