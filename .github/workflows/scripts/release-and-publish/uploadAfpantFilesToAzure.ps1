7190ac8575ef5d5a1e7e98adfc588e73d6b5ab82
Kristoffer Holm <kristoffer.holm@bits.no>
2025-05-12 21:16:50 +0200

    fix: Azure CDN test nr 6

--- .github/workflows/scripts/release-and-publish/uploadAfpantFilesToAzure.ps1
+++ .github/workflows/scripts/release-and-publish/uploadAfpantFilesToAzure.ps1
@@ -1,1 +1,8 @@
-ls
+New-Item -Path "./" -Name "actions-tmp" -ItemType "Directory"
+New-Item -Path "./actions-tmp" -Name "xslt" -ItemType "Directory"
+New-Item -Path "./actions-tmp" -Name "xsd" -ItemType "Directory"
+New-Item -Path "./actions-tmp" -Name "xml" -ItemType "Directory"
+Get-ChildItem -Path 'spesifikasjoner/afpant/' -Recurse -Include *.xslt | ForEach-Object -Process { Copy-Item $_.VersionInfo.FileName ./actions-tmp/xslt/ }
+Get-ChildItem -Path 'spesifikasjoner/afpant/' -Recurse -Include *.xsd | ForEach-Object -Process { Copy-Item $_.VersionInfo.FileName ./actions-tmp/xsd }
+Get-ChildItem -Path 'spesifikasjoner/afpant/' -Recurse -Include *.xml | ForEach-Object -Process { Copy-Item $_.VersionInfo.FileName ./actions-tmp/xml }
+Get-ChildItem -Path 'actions-tmp' -Recurse | ForEachObject -Process { Write-Output "::notice::Uploading file: $_.Name" }
