3c9c54cfb1603c27e3ebbcfa19f3cc3206c81889
Kristoffer Holm <kristoffer.holm@bits.no>
2025-05-12 17:02:57 +0200

    doc: Github Actions test 28

--- .github/workflows/scripts/release-and-publish/determineReleaseType.ps1
+++ .github/workflows/scripts/release-and-publish/determineReleaseType.ps1
@@ -1,7 +1,7 @@
 $CommitMessage = git log --format=%B -n 1 HEAD
 $RepositoryHost = $Env:RepositoryHost.Substring($Env:RepositoryHost.indexOf("/") + 1)
 $RepositoryURI = "$Env:RepositoryHost/$Env:RepositoryURI"
-$CurrentReleaseName = gh release view --repo $RepositoryURI | Select-Object -First 1
+$CurrentReleaseName = gh release view --repo $RepositoryURI
 
 Write-Output "::notice::Latest release in this repository was a release with name: $CurrentReleaseName"
 
@@ -14,6 +14,6 @@ if ( $CommitMessage -match '^doc:.*$') {
 } elseif ( $CommitMessage -match '^feat!:(\s|\S)*$' ) {
   Add-Content -Value "releaseType=breaking" -Path $Env:GITHUB_OUTPUT -Encoding utf8
 } else {
-  Write-Output "::error title=Naming-Convention-Error::The commit message did not follow the agreed upon naming convention"
+  Write-Output "::error title=Naming-Convention-Error::The commit message did not follow the agreed upon naming convention, the name of the lates commit was: $CommitMessage"
   exit 1
 }
