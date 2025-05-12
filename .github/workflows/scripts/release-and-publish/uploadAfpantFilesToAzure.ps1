8a609eb50e6d2fa270e629d64c5d56c6e593a672
Kristoffer Holm <kristoffer.holm@bits.no>
2025-05-12 21:19:20 +0200

    fix: Azure CDN test nr 7

--- .github/workflows/scripts/release-and-publish/uploadAfpantFilesToAzure.ps1
+++ .github/workflows/scripts/release-and-publish/uploadAfpantFilesToAzure.ps1
@@ -1,8 +1,8 @@
-New-Item -Path "./" -Name "actions-tmp" -ItemType "Directory"
-New-Item -Path "./actions-tmp" -Name "xslt" -ItemType "Directory"
-New-Item -Path "./actions-tmp" -Name "xsd" -ItemType "Directory"
-New-Item -Path "./actions-tmp" -Name "xml" -ItemType "Directory"
+New-Item -Path "./" -Name "actions-tmp" -ItemType "Directory" | Out-Null
+New-Item -Path "./actions-tmp" -Name "xslt" -ItemType "Directory" | Out-Null
+New-Item -Path "./actions-tmp" -Name "xsd" -ItemType "Directory" | Out-Null
+New-Item -Path "./actions-tmp" -Name "xml" -ItemType "Directory" | Out-Null
 Get-ChildItem -Path 'spesifikasjoner/afpant/' -Recurse -Include *.xslt | ForEach-Object -Process { Copy-Item $_.VersionInfo.FileName ./actions-tmp/xslt/ }
 Get-ChildItem -Path 'spesifikasjoner/afpant/' -Recurse -Include *.xsd | ForEach-Object -Process { Copy-Item $_.VersionInfo.FileName ./actions-tmp/xsd }
 Get-ChildItem -Path 'spesifikasjoner/afpant/' -Recurse -Include *.xml | ForEach-Object -Process { Copy-Item $_.VersionInfo.FileName ./actions-tmp/xml }
-Get-ChildItem -Path 'actions-tmp' -Recurse | ForEachObject -Process { Write-Output "::notice::Uploading file: $_.Name" }
+Get-ChildItem -Path 'actions-tmp' -Recurse | ForEach-Object -Process { Write-Output "::notice::Uploading file: $_.Name" }
