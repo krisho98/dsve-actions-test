29b8482110c22e5d6f91a33e23d63f33d00fb9c5
Kristoffer Holm <kristoffer.holm@bits.no>
2025-05-12 22:52:57 +0200

    feat: Azure CDN test nr 28

--- .github/workflows/scripts/release-and-publish/uploadAfpantFilesToAzure.ps1
+++ .github/workflows/scripts/release-and-publish/uploadAfpantFilesToAzure.ps1
@@ -5,4 +5,4 @@ New-Item -Path "./actions-tmp" -Name "xml" -ItemType "Directory" | Out-Null
 Get-ChildItem -Path 'spesifikasjoner/afpant/' -Recurse -Include *.xslt | ForEach-Object -Process { Copy-Item $_.VersionInfo.FileName ./actions-tmp/xslt/ }
 Get-ChildItem -Path 'spesifikasjoner/afpant/' -Recurse -Include *.xsd | ForEach-Object -Process { Copy-Item $_.VersionInfo.FileName ./actions-tmp/xsd }
 Get-ChildItem -Path 'spesifikasjoner/afpant/' -Recurse -Include *.xml | ForEach-Object -Process { Copy-Item $_.VersionInfo.FileName ./actions-tmp/xml }
-Get-ChildItem -Path 'actions-tmp' -Recurse | ForEach-Object -Process { Write-Output "::notice::Uploading file: $($_.Name.Substring($_.Name.lastIndexOf('/') + 1))" }
+Get-ChildItem -Path 'actions-tmp' -Recurse -Include "*.*" | ForEach-Object -Process { Write-Output "::notice::Uploading file: $($_.Name.Substring($_.Name.lastIndexOf('/') + 1))" }
