bafceed0b53307783d3c1c346148a9bc85168d7b
Kristoffer Holm <kristoffer.holm@bits.no>
2025-05-15 16:02:36 +0200

    feat!: Dette er en test av sanitizeren igjen, nr 3

--- .github/workflows/scripts/sanitize-commit/approve-pull-request.ps1
+++ .github/workflows/scripts/sanitize-commit/approve-pull-request.ps1
@@ -5,19 +5,19 @@ $PullRequestUrl = "$Env:gitHubHost/$Env:repoPath/$($mergeLessPRPath.Substring($m
 switch ($Env:releaseType) {
   doc {
     Write-Output "::notice::The latest commit indicates a documentation change. Merging this pull-request will not alter the version number."
-    gh pr review $PullRequestUrl --approve --comment "The naming is confirmed. The latest commit indicates a documentation change. Merging this pull-request will not alter the version number."
+    gh pr comment $PullRequestUrl --body "The naming is confirmed. The latest commit indicates a documentation change. Merging this pull-request will not alter the version number."
     exit 0
   } fix {
     Write-Output "::notice::The latest commit: $Env:commitMessage indicates a fix change. Merging this pull-request will increase the minor (0.0.x) version number."
-    gh pr review $PullRequestUrl --approve --comment "The naming is confirmed. The latest commit: $Env:commitMessage indicates a fix change. Merging this pull-request will increase the minor (0.0.x) version number."
+    gh pr comment $PullRequestUrl --body "The naming is confirmed. The latest commit: $Env:commitMessage indicates a fix change. Merging this pull-request will increase the minor (0.0.x) version number."
     exit 0
   } feat {
     Write-Output "::notice::The latest commit: $Env:commitMessage indicates a non-breaking feature change. Merging this pull-request will increase the middle (0.x.0) version number."
-    gh pr review $PullRequestUrl --approve --comment "The naming is confirmed. The latest commit: $Env:commitMessage indicates a non-breaking feature change. Merging this pull-request will increase the middle (0.x.0) version number."
+    gh pr comment $PullRequestUrl --body "The naming is confirmed. The latest commit: $Env:commitMessage indicates a non-breaking feature change. Merging this pull-request will increase the middle (0.x.0) version number."
     exit 0
   } breaking {
     Write-Output "::notice::The latest commit: $Env:commitMessage indicates a breaking feature change. Merging this pull-request will increase the major (x.0.0) version number."
-    gh pr review $PullRequestUrl --approve --comment "The naming is confirmed. The latest commit: $Env:commitMessage indicates a breaking feature change. Merging this pull-request will increase the major (x.0.0) version number."
+    gh pr comment $PullRequestUrl --body "The naming is confirmed. The latest commit: $Env:commitMessage indicates a breaking feature change. Merging this pull-request will increase the major (x.0.0) version number."
     exit 0
   }
 }
