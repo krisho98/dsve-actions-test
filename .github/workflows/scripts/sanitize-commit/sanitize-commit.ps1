61f9a50dda3ad2b06cf07a8931380387ecb601f8
Kristoffer Holm <kristoffer.holm@bits.no>
2025-05-15 11:45:28 +0200

    doc: Ny sanitizer test nr 1

---
+++ .github/workflows/scripts/sanitize-commit/sanitize-commit.ps1
@@ -0,0 +1,18 @@
+Write-Output $Env:trigger
+$CommitMessage = git log --format=%B -n 1 HEAD
+if ( $CommitMessage -match '^doc:(\s|\S)*$') {
+  Write-Output "::notice::The latest commit indicates a documentation change. Merging this pull-request will not alter the version number."
+  exit 0
+} elseif ( $CommitMessage -match '^fix:(\s|\S)*$' ) {
+  Write-Output "::notice::The latest commit indicates a fix change. Merging this pull-request will increase the minor (0.0.x) version number."
+  exit 0
+} elseif ( $CommitMessage -match '^feat:(\s|\S)*$' ) {
+  Write-Output "::notice::The latest commit indicates a non-breaking feature change. Merging this pull-request will increase the middle (0.x.0) version number."
+  exit 0
+} elseif ( $CommitMessage -match '^feat!:(\s|\S)*$' ) {
+  Write-Output "::notice::The latest commit indicates a breaking feature change. Merging this pull-request will increase the major (x.0.0) version number."
+  exit 0
+} else {
+  Write-Output "::error title=Naming-Convention-Error::The commit message did not follow the agreed upon naming convention, the name of the latest commit was: $CommitMessage"
+  exit 1
+}
